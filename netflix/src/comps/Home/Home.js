c63b5f349b264df2ab5b6a68eb4c05d4175cb56a
Hannah. g <hannagebrehiwet3@gmail.com>
2024-01-31 09:13:45 -0800

    rows

--- netflix/src/comps/Home/Home.js
+++ netflix/src/comps/Home/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
-
+import Banner from '../../components/banner/Banner'
+import Rowlist from '../../components/rows/Rowlist/Rowlist'
 function Home () {
   return (
     <div className="App">
       <Header />
+      <Banner />
+      <Rowlist />
       <Footer/>
     </div>
   )
